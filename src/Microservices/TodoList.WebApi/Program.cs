464a7bd089628c79732af14fa7111089c774c365
Kelvin Castro <kelvincastroguzman@gmail.com>
2025-06-03 17:26:16 +0200

    Implementation of KeyVault and added Microsoft.Extensions.Configuration.AzureKeyVault nuget

--- src/Microservices/TodoList.WebApi/Program.cs
+++ src/Microservices/TodoList.WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using TodoList.Application.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -30,6 +33,18 @@ app.Run();
 
 static string GetDbConnectionString(WebApplicationBuilder builder)
 {
-    string todoListDbConnectionString = "Server=KELVINCASTRO; Database=TodoListDB; User Id=ke; Password=12345678; MultipleActiveResultSets=True; Encrypt=false; Connection Timeout=500;";
+    IConfigurationSection keyVaultKeyVaultUrl = builder.Configuration.GetSection("KeyVault:KeyVaultUrl");
+    IConfigurationSection keyVaultClientId = builder.Configuration.GetSection("KeyVault:ClientId");
+    IConfigurationSection keyVaultClientSecret = builder.Configuration.GetSection("KeyVault:ClientSecret");
+    IConfigurationSection keyVaultDirectoryID = builder.Configuration.GetSection("KeyVault:DirectoryID");
+
+    var credential = new ClientSecretCredential(keyVaultDirectoryID.Value, keyVaultClientId.Value, keyVaultClientSecret.Value);
+
+    builder.Configuration.AddAzureKeyVault(keyVaultKeyVaultUrl.Value, keyVaultClientId.Value, keyVaultClientSecret.Value, new DefaultKeyVaultSecretManager());
+
+    var client = new SecretClient(new Uri(keyVaultKeyVaultUrl.Value), credential);
+
+    string todoListDbConnectionString = client.GetSecret("TodoListDB-ConnectionString").Value.Value;
+
     return todoListDbConnectionString;
 }
